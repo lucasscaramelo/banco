26aa2d92f8ac7809fcc1c01c39a6b18f62da101a
Lucas Scaramelo <lucas.scaramelo@gmail.com>
2021-03-12 20:49:17 -0300

    Retirada da classe pessoas e inclusao das classes de testes

--- src/test/java/br/com/banco/ClienteTest.java
+++ src/test/java/br/com/banco/ClienteTest.java
@@ -1,4 +1,16 @@
 package br.com.banco;
 
+import org.junit.jupiter.api.Test;
+
 public class ClienteTest {
+
+    @Test
+    public void clienteValido() {
+
+    }
+
+    @Test
+    public void clienteInvalido() {
+
+    }
 }
--- src/test/java/br/com/banco/PessoaTest.java
+++
@@ -1,4 +0,0 @@
-package br.com.banco;
-
-public class PessoaTest {
-}
